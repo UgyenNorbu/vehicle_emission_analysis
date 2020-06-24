8b088678941163bc018d2cca780e9da5ef2e7177
Ugyen Norbu <unorbu2019@gmail.com>
2020-06-24 20:48:36 +0600

    commit

--- veh_emission.R
+++ veh_emission.R
@@ -1,1 +1,27 @@
-fdsadsf
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+
+data <- read_xlsx("data/EmissionTest_Details_Jan-Dec2019.xlsx")
+
+data <- data %>% 
+    mutate(Test_Result = ifelse(Test_Result == 1, "PASS", Test_Result)) %>%
+    mutate(Test_Result = ifelse(Test_Result == "null" & Engine_Name == "Petrol" & (CO < 4),
+                                "PASS", Test_Result)) %>% 
+    filter(!is.na(Test_Result)) %>% 
+    filter(Test_Result != 'null')
+
+data_grouped <- data %>% 
+    group_by(region_name, Test_Result) %>% 
+    tally(name = "veh_num")
+
+data_grouped %>% 
+    ggplot(aes(reorder(x = region_name, -veh_num), y = veh_num, 
+               fill = Test_Result)) +
+    geom_bar(stat = "identity", position=position_dodge2(reverse = TRUE)) +
+    labs(x = "", 
+         y = "Number of vehicles") +
+    theme_linedraw() +
+    theme(legend.position = "none") +
+    coord_flip() +
+    ylim(0, 40000)
